4884cc3572209ce2f05d2a6ee4eab7f4775b17ce
nayakseema <nayakseema08@gmail.com>
2022-06-10 11:12:51 +0000

    db project

---
+++ src/test/deletedb.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class deletedb {
+public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		String qry = "select * from employees";
+	    Class.forName("com.mysql.cj.jdbc.Driver");
+	    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "root");
+	    Statement stm = conn.createStatement();
+	    stm.executeUpdate("delete from eproduct where id = 2");
+	    ResultSet result = stm.executeQuery("select * from eproduct");
+	    while(result.next()) {
+	    	System.out.println(result.getString("name"));
+	    }
+	    conn.close();
+	}
+
+}
